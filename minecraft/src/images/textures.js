6f00fe3d560f59f7b418ee83915d283393906edb
fb0801 <fbhatti0801@gmail.com>
2023-10-24 17:12:23 +0100

    hover and save world

--- minecraft/src/images/textures.js
+++ minecraft/src/images/textures.js
@@ -15,7 +15,14 @@ const glassTexture = new TextureLoader().load(glassImg)
 const woodTexture = new TextureLoader().load(woodImg)
 const groundTexture = new TextureLoader().load(grassImg)
 
-
+dirtTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+grassTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+groundTexture.magFilter = NearestFilter;
+groundTexture.wrapS = RepeatWrapping
+groundTexture.wrapT = RepeatWrapping
 
 export {
 	dirtTexture,
